c998d37ee7d498855429c5d7416716d8c11ca24b
Manpreet Dhaliwal <manip426@gmail.com>
2021-09-03 11:31:56 -0700

    updated chat.js with firebase credentails, didmount and willmount

--- components/Chat.js
+++ components/Chat.js
@@ -5,85 +5,154 @@ import {
   KeyboardAvoidingView,
   Platform,
   Text,
+  FlatList,
 } from "react-native";
+
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import firebase from "firebase"; //import firebase to fetch the data from firebase Database
+import firestore from "firebase";
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       messages: [],
+      user: {
+        _id: "",
+        name: "",
+        avatar: "",
+      },
+      uid: 0,
+      loggedInText: "Please wait, you are getting logged in",
     };
+    if (!firebase.apps.length) {
+      // firebase credentials
+      firebase.initializeApp({
+        apiKey: "AIzaSyDSgFiVJ2933nxF9SBotfer6C_wsvC68uo",
+        authDomain: "lets-chat-1657c.firebaseapp.com",
+        projectId: "lets-chat-1657c",
+        storageBucket: "lets-chat-1657c.appspot.com",
+        messagingSenderId: "917168535481",
+        appId: "1:917168535481:web:5096ef425ea8385a99e868",
+        measurementId: "G-F53M525R80",
+      });
+    }
+
+    this.referenceChatMessagesUser = null;
   }
+
+
   componentDidMount() {
     let name = this.props.route.params.name;
 
     this.props.navigation.setOptions({ title: "Hi " + name });
 
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: "Hello developer ",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any",
-          },
-        },
-        {
-          _id: 2,
-          text: "This is a system message",
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
-      name: name,
+    //creating a references to messages collection- brings the data from the collection "messages", this.referenceChatMessages will receive the data and database updates
+    this.referenceChatMessages = firebase.firestore().collection("messages");
+
+    // listen to authentication events
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+      if (!user) {
+        await firebase.auth().signInAnonymously();
+      }
+      //update user state with currently active user data
+      this.setState({
+        uid: user.uid,
+        loggedInText: "Hello there",
+      });
+
+      // create a reference to the active user's documents (shopping lists)
+      this.referenceChatMessagesUser = firebase
+        .firestore()
+        .collection("messages")
+        .where("uid", "==", this.state.uid);
+      // listen for collection changes for current user
+
+      this.unsubscribe = this.referenceChatMessages
+        //orderBy sort the documents (messages) by date
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
     });
   }
 
-  onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
+  componentWillUnmount() {
+    // stop listening to authentication
+    this.authUnsubscribe();
+    // stop listening for changes
+    this.unsubscribe();
   }
 
-  renderBubble(props) {
-    return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          right: {
-            backgroundColor: "#000",
-          },
-        }}
-      />
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        user: data.user,
+        system: data.system,
+        createdAt: data.createdAt.toDate(),
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
+
+  sendMessage(messages) {
+    this.referenceChatMessages.add(messages[0]); // on send add the message[0] to the firebase then it automaticaly will be fetched
+  }
+  onSend(messages = []) {
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.sendMessage(messages);
+      }
     );
   }
 
-  render() {
-    let chatColor = this.props.route.params.color;
 
-    return (
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? null : null}
-        style={{ flex: 1, justifyContent: "flex-end" }}
-      >
-        <GiftedChat
-          listViewProps={{
-            style: {
-              backgroundColor: chatColor,
+    renderBubble(props) {
+      return (
+        <Bubble
+          {...props}
+          wrapperStyle={{
+            right: {
+              backgroundColor: "#000",
             },
-          }} // listVieProps allows me to change the color background of the GiftedChat
-          renderBubble={this.renderBubble.bind(this)} //changes the color of the chat bubble
-          messages={this.state.messages}
-          onSend={(messages) => this.onSend(messages)}
-          user={{
-            _id: 1,
           }}
         />
-      </KeyboardAvoidingView> // If I add a view in as a parent of the GiftedChat, this last does not render
-    );
-  }
-}
+      );
+    }
+  render() {
+     let chatColor = this.props.route.params.color;
+
+     return (
+       <KeyboardAvoidingView
+         behavior={Platform.OS === "ios" ? null : null}
+         style={{ flex: 1, justifyContent: "flex-end" }}
+       >
+         <Text>{this.state.loggedInText}</Text>
+         <GiftedChat
+           listViewProps={{
+             style: {
+               backgroundColor: chatColor,
+             },
+           }} // listVieProps allows me to change the color background of the GiftedChat
+           renderBubble={this.renderBubble.bind(this)} //changes the color of the chat bubble
+           messages={this.state.messages}
+           onSend={(messages) => this.onSend(messages)}
+           user={{
+             _id: 1,
+             name: "pedro",
+             avatar: "https://placeimg.com/140/140/any",
+           }}
+         />
+       </KeyboardAvoidingView> // If I add a view in as a parent of the GiftedChat, this last does not render
+     );
+   }
+ }
